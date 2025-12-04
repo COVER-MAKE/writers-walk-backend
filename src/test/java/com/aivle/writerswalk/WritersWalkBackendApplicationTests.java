de6b9cdb4a821c142abfe3747656003b29b0fd5e
SinnoLn <sintmtn69@gmail.com>
2025-12-04 11:09:56 +0900

    initial commit

---
+++ src/test/java/com/aivle/writerswalk/WritersWalkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aivle.writerswalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WritersWalkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
