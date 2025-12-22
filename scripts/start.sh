024fecc0f0c82244d93289b8365260495343645b
SinnoLn <sintmtn69@gmail.com>
2025-12-23 05:26:28 +0900

    fix: buildspec파일 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -e
 
+echo "=== DEBUGGING START ==="
+ls -al /etc/systemd/system/writerswalk.service || echo ">>> [ERROR] Service file is MISSING at destination!"
+
+ls -al /home/ubuntu/writerswalk.service || echo ">>> [WARNING] Source file not found in deployment root"
+echo "=== DEBUGGING END ==="
+
 echo "Reload systemd and restart writerswalk"
 systemctl daemon-reload
+systemctl enable writerswalk || true
 systemctl restart writerswalk
 systemctl status writerswalk --no-pager
